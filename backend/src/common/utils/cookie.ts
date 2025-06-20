00cb3ccff8806926b4fc9b15650deeb1b53599ee
Selim <selim@technovicinity.com>
2025-06-20 12:23:09 +0600

    cookie option

--- backend/src/common/utils/cookie.ts
+++ backend/src/common/utils/cookie.ts
@@ -12,8 +12,9 @@ export const REFRESH_PATH = `${config.BASE_PATH}/auth/refresh`;
 
 const defaults: CookieOptions = {
   httpOnly: true,
-  //secure: config.NODE_ENV === "production" ? true : false,
-  //sameSite: config.NODE_ENV === "production" ? "strict" : "lax",
+  secure: config.NODE_ENV === "production" ? true : false,
+  // sameSite: config.NODE_ENV === "production" ? "strict" : "lax",
+  sameSite: "lax",
 };
 
 export const getRefreshTokenCookieOptions = (): CookieOptions => {
